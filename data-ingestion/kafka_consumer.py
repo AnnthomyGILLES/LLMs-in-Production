6266f4b1c995c5424639c5f196b6603c7221410e
AnnthomyGILLES <annthomy.gilles@gmail.com>
2024-07-27 23:54:16 -0400

    Minor changes

--- data-ingestion/kafka_consumer.py
+++ data-ingestion/kafka_consumer.py
@@ -7,7 +7,7 @@ from kafka import KafkaConsumer
 class KafkaConsumerWrapper:
     def __init__(self, bootstrap_servers=None, topic='incoming-data', group_id='my-group'):
         if bootstrap_servers is None:
-            bootstrap_servers = ['localhost:9093']
+            bootstrap_servers = ['kafa:9092']
         self.consumer = KafkaConsumer(
             topic,
             bootstrap_servers=bootstrap_servers,
