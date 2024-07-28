a20ce319e0df0fc74b9c0be923527fea3b0d7e6c
AnnthomyGILLES <annthomy.gilles@gmail.com>
2024-07-27 20:01:05 -0400

    Rename database

--- data-crawling/config.py
+++ data-crawling/config.py
@@ -5,8 +5,9 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file="../.env", env_file_encoding="utf-8")
 
     # MongoDB configs
-    MONGO_DATABASE_HOST: str = "mongodb://localhost:30001,localhost:30002,localhost:30003/?replicaSet=my-replica-set"
-    MONGO_DATABASE_NAME: str = "scrabble"
+    # MONGO_DATABASE_HOST: str = "mongodb://localhost:30001,localhost:30002,localhost:30003/?replicaSet=my-replica-set"
+    MONGO_DATABASE_HOST: str = "mongodb://localhost:27017/"
+    MONGO_DATABASE_NAME: str = "llmtoprod_db"
 
     LINKEDIN_USERNAME: str | None = None
     LINKEDIN_PASSWORD: str | None = None
