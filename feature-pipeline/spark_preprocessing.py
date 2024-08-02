4497d81a5e16cc40a7f0b391f5bf8a140eec4232
AnnthomyGILLES <annthomy.gilles@gmail.com>
2024-08-02 01:07:32 -0400

    Add spark preprocessing first steps

--- feature-pipeline/spark_preprocessing.py
+++ feature-pipeline/spark_preprocessing.py
@@ -1,33 +1,46 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
+from pyspark.sql.functions import from_json, col, split, size, avg
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
-# Define the schema of the incoming data
-schema = StructType([
-    StructField("id", IntegerType(), True),
-    StructField("name", StringType(), True),
-    StructField("address", StringType(), True),
-    StructField("email", StringType(), True),
-    StructField("phone_number", StringType(), True),
-    StructField("post", StringType(), True)
-])
+if __name__ == '__main__':
+    # Define the schema of the incoming data
+    schema = StructType([
+        StructField("id", IntegerType(), True),
+        StructField("name", StringType(), True),
+        StructField("address", StringType(), True),
+        StructField("email", StringType(), True),
+        StructField("phone_number", StringType(), True),
+        StructField("post", StringType(), True)
+    ])
 
-# Create SparkSession
-spark = SparkSession.builder.appName("KafkaSparkStreaming").getOrCreate()
+    # Create SparkSession
+    spark = SparkSession.builder.appName("KafkaSparkStreaming").getOrCreate()
 
-# Read from Kafka
-df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "kafka:9092").option("subscribe",
-                                                                                             "my-topic").load()
+    # Read from Kafka
+    df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "redpanda:29092").option("subscribe",
+                                                                                                     "my-topic").load()
 
-# Parse the JSON data
-parsed_df = df.select(
-    from_json(col("value").cast("string"), schema).alias("data")
-).select("data.*")
+    # Parse JSON data
+    parsed_df = df.select(
+        from_json(col("value").cast("string"), schema).alias("data")
+    ).select("data.*")
 
-# Process the data (example: count by name)
-processed_df = parsed_df.groupBy("name").count()
+    # Count words per post
+    word_count_df = parsed_df.select(
+        "id",
+        "name",
+        "post",
+        size(split(col("post"), "\\s+")).alias("word_count")
+    )
 
-# Write the output to console (for demonstration)
-query = processed_df.writeStream.outputMode("complete").format("console").start()
+    # Compute average word count
+    avg_word_count = word_count_df.select(avg("word_count").alias("avg_word_count"))
 
-query.awaitTermination()
+    # Write the output
+    query = avg_word_count \
+        .writeStream \
+        .outputMode("complete") \
+        .format("console") \
+        .start()
+
+    query.awaitTermination()
