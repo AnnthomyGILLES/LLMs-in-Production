02679f8d8ad5f60a4137ff21b3649f7e404931da
AnnthomyGILLES <annthomy.gilles@gmail.com>
2024-08-10 18:05:57 -0400

    Fix kafka server

--- feature-pipeline/transformations/storing.py
+++ feature-pipeline/transformations/storing.py
@@ -4,14 +4,14 @@ from qdrant_client import models, QdrantClient
 from qdrant_client.grpc import VectorParams
 from sentence_transformers import SentenceTransformer
 
-from kafka_consumer import KafkaConsumerWrapper
+from fake_kafka_consumer import KafkaConsumerWrapper
 
 
 def process_and_insert_to_qdrant():
     # Kafka Consumer setup
     bootstrap_servers = ['redpanda:29092']
     kafka_topic = 'output-spark-topic'
-    consumer = KafkaConsumerWrapper(bootstrap_servers, kafka_topic, group_id='qdrant-consumer-group')
+    consumer = KafkaConsumerWrapper(bootstrap_servers, kafka_topic)
 
     # Qdrant setup
     encoder = SentenceTransformer("all-MiniLM-L6-v2")
